07ffbed855575546fef923d749a2b7aed0da3bfc
dontmindaditya <aditsingh8077@gmail.com>
2026-07-08 02:32:05 -0700

    first

---
+++ Javascript/NaN.js
@@ -0,0 +1,5 @@
+//What is NaN property in JavaScript?
+
+//NaN property represents the “Not-a-Number” value. It indicates a value that is not a legal number.
+//typeof of NaN will return a Number.
+//To check if a value is NaN, we use the isNaN() function,
