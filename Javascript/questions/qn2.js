d1a3415fbd054dda1b66772609e3d141bb8933cd
dontmindaditya <aditsingh8077@gmail.com>
2026-07-08 10:13:06 -0700

    first

---
+++ Javascript/questions/qn2.js
@@ -0,0 +1,5 @@
+//What do mean by prototype design pattern?
+
+//The Prototype Pattern produces different objects, but instead of returning uninitialized objects, it produces objects that have values replicated from a template – or sample – object. Also known as the Properties pattern, the Prototype pattern is used to create prototypes
+//The introduction of business objects with parameters that match the database's default settings is a good example of where the Prototype pattern comes in handy. The default settings for a newly generated business object are stored in the prototype object.
+//The Prototype pattern is hardly used in traditional languages, however, it is used in the development of new objects and templates in JavaScript, which is a prototypal language
