39406d405d3aef882a33d2890e8a302112fe55f6
dontmindaditya <aditsingh8077@gmail.com>
2026-07-09 11:05:50 -0700

    second

---
+++ Javascript/advance/qn5.js
@@ -0,0 +1,8 @@
+//What are TypedArrays and when would you use them?
+
+//TypedArrays store binary data in a fixed format. 
+// They are faster and more memory-efficient than normal arrays. 
+// They are used in cases like graphics, audio, or handling raw data.
+//  Values must match the defined type. This gives better performance.
+
+Example: const buffer = new Uint8Array(8);
