01b8399a0690869b418a854e252045de4a667615
dontmindaditya <aditsingh8077@gmail.com>
2026-07-09 03:44:17 -0700

    second

---
+++ Javascript/questions/qn5.js
@@ -0,0 +1,11 @@
+//In JavaScript, how many different methods can you make an object?
+
+//In JavaScript, there are several ways to declare or construct an object.
+
+    //Object.
+    //using Class.
+    //create Method.
+    //Object Literals.
+    //using Function.
+    //Object Constructor.
+
