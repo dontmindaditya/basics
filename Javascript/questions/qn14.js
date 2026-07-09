b87e6a597d1367880e3749c86bdb0a76b5dad562
dontmindaditya <aditsingh8077@gmail.com>
2026-07-09 09:29:56 -0700

    second

---
+++ Javascript/questions/qn14.js
@@ -0,0 +1,14 @@
+//What is a Temporal Dead Zone?
+
+//Temporal Dead Zone is a behaviour that occurs with variables declared using let and const keywords. It is a behaviour where we try to access a variable before it is initialized. Examples of temporal dead zone
+
+x = 23; // Gives reference error
+
+let x;
+
+function anotherRandomFunc(){
+  message = "Hello"; // Throws a reference error
+
+  let message;
+}
+anotherRandomFunc();
