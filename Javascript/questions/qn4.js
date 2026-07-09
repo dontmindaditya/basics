46e0ab0d3191c4424f7b639fd0868eeb31994b7b
dontmindaditya <aditsingh8077@gmail.com>
2026-07-09 03:41:50 -0700

    second

---
+++ Javascript/questions/qn4.js
@@ -0,0 +1,11 @@
+//What is the rest parameter and spread operator?
+
+//Both rest parameter and spread operator were introduced in the ES6 version of javascript.
+
+//Rest parameter ( … ):
+
+    //It provides an improved way of handling the parameters of a function.
+    //Using the rest parameter syntax, we can create functions that can take a variable number of arguments.
+    //Any number of arguments will be converted into an array using the rest parameter.
+    //It also helps in extracting all or some parts of the arguments.
+    //Rest parameters can be used by applying three dots (...) before the parameters.
