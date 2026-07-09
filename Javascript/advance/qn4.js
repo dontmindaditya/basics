6dfad0bd9944d698b51d499dbdcd7962987548d9
dontmindaditya <aditsingh8077@gmail.com>
2026-07-09 11:00:08 -0700

    second

---
+++ Javascript/advance/qn4.js
@@ -0,0 +1,5 @@
+//How does structured clone differ from JSON serialization?
+
+//Structured clones can copy complex objects like Maps and Sets. JSON serialization only works with basic data types. Structured clones also handle circular references. 
+// It is used internally by APIs like postMessage.
+//  JSON is more limited but widely supported.
