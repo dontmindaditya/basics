ee00aa48540420c1cead0cf565a65c4d513bd147
dontmindaditya <aditsingh8077@gmail.com>
2026-07-09 09:34:59 -0700

    second

--- Javascript/questions/qn15.js
+++ Javascript/questions/qn15.js
@@ -1,1 +1,13 @@
 //What do you mean by JavaScript Design Patterns?
+
+//JavaScript design patterns are repeatable approaches for errors that arise sometimes when building JavaScript browser applications. They truly assist us in making our code more stable.
+
+//They are divided mainly into 3 categories 
+
+    //Creational Design Pattern
+    //Structural Design Pattern
+    //Behavioral Design Pattern.
+
+// Creational Design Pattern: The object generation mechanism is addressed by the JavaScript Creational Design Pattern. They aim to make items that are appropriate for a certain scenario.
+//Structural Design Pattern: The JavaScript Structural Design Pattern explains how the classes and objects we've generated so far can be combined to construct bigger frameworks. This pattern makes it easier to create relationships between items by defining a straightforward way to do so.
+//Behavioral Design Pattern: This design pattern highlights typical patterns of communication between objects in JavaScript. As a result, the communication may be carried out with greater freedom.
